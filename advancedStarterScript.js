0d3a4872655df97c9453fbab9f52261dfa285123
Tyler Amano-Smerling <tyleramano-smerling@MacBook-Pro-6.local>
2017-01-04 16:07:03 -0800

    Save remaining files in directory just in case, transfer remaining code from old verison, and fix moving bug

--- advancedStarterScript.js
+++ advancedStarterScript.js
@@ -12,42 +12,55 @@ function printBoard(){
 		for(var j = 0; j < 4; j++){
 			var boardID = "r"+i+"c"+j;
 			//if the tile is not zero, put it on the board 
-			if(board[i][j]!=0){
 				document.getElementById(boardID).innerHTML = board[i][j];
-			}
 			//Change the different number tiles to different colors
 			switch(board[i][j]){
+				case 0:
+					document.getElementById(boardID).style.color = "#CDC1B5";
+					document.getElementById(boardID).style.background = "#CDC1B5";
+					break;
 				case 2:
+					document.getElementById(boardID).style.color = "#ffffff";
 					document.getElementById(boardID).style.background = "#f0e5da";
 					break;//similar to an else if. Makes sure none of the other cases executes if this one does
 				case 4:
+				document.getElementById(boardID).style.color = "#ffffff";
 					document.getElementById(boardID).style.background = "#ede2c8";
 					break;
 				case 8:
+					document.getElementById(boardID).style.color = "#ffffff";
 					document.getElementById(boardID).style.background = "#feb578";
 					break;
 				case 16:
+					document.getElementById(boardID).style.color = "#ffffff";
 					document.getElementById(boardID).style.background = "#ff9962";
 					break;
 				case 32:
+					document.getElementById(boardID).style.color = "#ffffff";
 					document.getElementById(boardID).style.background = "#ff8060";
 					break;
 				case 64:
+					document.getElementById(boardID).style.color = "#ffffff";
 					document.getElementById(boardID).style.background = "#ff613c";
 					break;
 				case 128:
+					document.getElementById(boardID).style.color = "#ffffff";
 					document.getElementById(boardID).style.background = "#efd26d";
 					break;
 				case 256:
+					document.getElementById(boardID).style.color = "#ffffff";
 					document.getElementById(boardID).style.background = "#efd15c";
 					break;
 				case 512:
+					document.getElementById(boardID).style.color = "#ffffff";
 					document.getElementById(boardID).style.background = "#efcd4a";
 					break;
 				case 1024:
+					document.getElementById(boardID).style.color = "#ffffff";
 					document.getElementById(boardID).style.background = "#f0ca36";
 					break;
 				case 2048:
+					document.getElementById(boardID).style.color = "#ffffff";
 					document.getElementById(boardID).style.background = "#ccc0b3";
 					break;
 				default:
@@ -71,23 +84,23 @@ document.onkeydown = function(e){
 
 	if (e.keyCode == UP_ARROW) {
         // up arrow
-        //combineTilesUp();
+        combineTilesUp();
         moveTilesUp();
     }
     //double equals sign will convert it for us 
     else if (e.keyCode == DOWN_ARROW) {
         // down arrow
-         //combineTilesDown();
+         combineTilesDown();
          moveTilesDown();
     }
     else if (e.keyCode == LEFT_ARROW) {
        // left arrow
-       //combineTilesLeft();
+       combineTilesLeft();
        moveTilesLeft();
     }
     else if (e.keyCode == RIGHT_ARROW) {
        // right arrow
-       //combineTilesRight();
+       combineTilesRight();
        moveTilesRight();
     } 
     addTile();
@@ -113,7 +126,73 @@ function addTile() {
 
 };
 
+function combineTilesDown(){
+    for(var r=0; r < board.length; r++)
+    {
+        for(var c=0; c<board[r].length; c++)
+        {
+            if(r!=0 && board[r][c] != 0 && board[r][c] == board[r-1][c])
+            {
+                board[r-1][c] = board[r][c]+board[r-1][c];
+                board[r][c] = 0;
+            }
 
+        }
+        
+    }   
+
+};
+
+function combineTilesUp(){
+    for(var r=board.length-1; r >= 0; r--)
+    {
+        for(var c=0; c<board[r].length; c++)
+        {
+            if(r != board.length-1 && board[r][c] != 0 && board[r][c] == board[r+1][c])
+            {
+                board[r+1][c] = board[r][c]+board[r+1][c];
+                board[r][c] = 0;
+            }
+
+        }
+        
+    }   
+
+};
+
+function combineTilesLeft(){
+    for(var r=0; r < board.length; r++)
+    {
+        for(var c=0; c < board[r].length-1; c++)
+        {
+            if(c != board[r].length-1 && board[r][c] != 0 && board[r][c] == board[r][c+1])
+            {
+                board[r][c] = board[r][c]+board[r][c+1];
+                board[r][c+1] = 0;
+            }
+
+        }
+        
+    }   
+
+};
+
+function combineTilesRight(){
+    for(var r=0; r < board.length; r++)
+    {
+        for(var c=board[r].length-1; c >= 0; c--)
+        {
+            if(c != 0 && board[r][c] != 0 && board[r][c] == board[r][c-1])
+            {
+                board[r][c] = board[r][c]+board[r][c-1];
+                board[r][c-1] = 0;
+            }
+
+        }
+        
+    }   
+
+};
 function moveTilesDown()
 {
     
@@ -123,7 +202,6 @@ function moveTilesDown()
         {
             if(r != board.length-1  && board[r][c] != 0 && board[r+1][c] == 0)
             {
-                console.log("Moving down!");
                 board[r+1][c] = board[r][c];
                 board[r][c] = 0;
 
@@ -182,6 +260,7 @@ function moveTilesRight()
             {
                 board[r][c+1] = board[r][c];
                 board[r][c] = 0;
+                console.log(board[r][c]);
             }
 
             
--- starterscript.js
+++ starterscript.js
@@ -3,34 +3,35 @@ var UP_ARROW = '38';
 var DOWN_ARROW = '40';
 var LEFT_ARROW = '37';
 var RIGHT_ARROW = '39';
+var emptySpacesLeft = 15;
 
 
 //As soon as webpage loads run these two functions
 $(document).ready(function(){
-	setUpBoard();
-	printBoard();
-	console.log("Loaded webpage"); //how you do print statements in javascript
+    setUpBoard();
+    printBoard();
+    console.log("Loaded webpage"); //how you do print statements in javascript
 });
 
 function setUpBoard(){
 
-	// initialize board to have no values
-	for(var i=0; i<4; i++){
-		var innergrid = [];
-		for(var j=0; j<4; j++){
-			innergrid.push("x");
-		}
-		grid.push(innergrid);
-	}
-	
-	addTile();
-	
+    // initialize board to have no values
+    for(var i=0; i<4; i++){
+        var innergrid = [];
+        for(var j=0; j<4; j++){
+            innergrid.push("x");
+        }
+        grid.push(innergrid);
+    }
+
+    addTile();
+    
 }
 
 function addTile() {
-	//place a 2 on a random spot in the board	
-	var x = Math.round(Math.random()*3);
-	var y = Math.round(Math.random()*3);
+    //place a 2 on a random spot in the board   
+    var x = Math.round(Math.random()*3);
+    var y = Math.round(Math.random()*3);
     var numbers = ["2","4"];
     var tile = numbers[Math.floor(Math.random() * numbers.length)];
     if(Math.random()<0.8){
@@ -40,30 +41,35 @@ function addTile() {
         tile = "4";
         var value = 4;
     }
+    if(emptySpacesLeft > 0){
     while(grid[x][y] !== "x"){
         x = Math.round(Math.random()*3);
         y = Math.round(Math.random()*3);
     }
+} else {
+    console.log("You lose!");
+}
 
     grid[x][y] = tile;
+    emptySpacesLeft--;
 
 }
-	
+    
 
 function printBoard(){
-	var board = '<br/>' + "----------------" + '<br/>';
-	for(var i=0; i<grid.length; i++){
-		board += "|   ";
-		for(var j=0; j<grid[i].length; j++){
-			board += grid[i][j] + "   |   ";
-		}
-		board += '<br/>';
-		board += "----------------";
-		board += '<br/>';
-	}
-	
-	//console.log(board);
-	document.getElementById("container").innerHTML = board;
+    var board = '<br/>' + "----------------" + '<br/>';
+    for(var i=0; i<grid.length; i++){
+        board += "|   ";
+        for(var j=0; j<grid[i].length; j++){
+            board += grid[i][j] + "   |   ";
+        }
+        board += '<br/>';
+        board += "----------------";
+        board += '<br/>';
+    }
+    
+    //console.log(board);
+    document.getElementById("container").innerHTML = board;
 }
 
 
@@ -86,19 +92,19 @@ document.onkeydown = function(e) {
     //double equals sign will convert it for us 
     else if (e.keyCode == DOWN_ARROW) {
         // down arrow
-        console.log("Pressed down");
+        console.log(emptySpacesLeft);
         combineTilesDown();
         moveTilesDown();
     }
     else if (e.keyCode == LEFT_ARROW) {
        // left arrow
-       console.log("Pressed left");
+       console.log(emptySpacesLeft);
        combineTilesLeft();
        moveTilesLeft();
     }
     else if (e.keyCode == RIGHT_ARROW) {
        // right arrow
-       console.log("Pressed right");
+       console.log(emptySpacesLeft);
        combineTilesRight();
        moveTilesRight();
     } 
@@ -106,7 +112,7 @@ document.onkeydown = function(e) {
     addTile();
     printBoard(); //have to recall print board to get the board to update
 };
-function combineTilesUp(){
+function combineTilesDown(){
     for(var r=0; r < grid.length; r++)
     {
         for(var c=0; c<grid[r].length; c++)
@@ -116,6 +122,7 @@ function combineTilesUp(){
                 var tileNumber = parseInt(grid[r][c]) + parseInt(grid[r-1][c]);
                 grid[r-1][c] = tileNumber + "";
                 grid[r][c] = "x";
+                emptySpacesLeft++;
             }
 
         }
@@ -124,7 +131,7 @@ function combineTilesUp(){
 
 }
 
-function combineTilesDown(){
+function combineTilesUp(){
     for(var r=grid.length-1; r >= 0; r--)
     {
         for(var c=0; c<grid[r].length; c++)
@@ -132,8 +139,9 @@ function combineTilesDown(){
             if(r !== grid.length-1 && grid[r][c] !== "x" && grid[r][c] == grid[r+1][c])
             {
                 var tileNumber = parseInt(grid[r][c]) + parseInt(grid[r+1][c]);
-                grid[r][c] = tileNumber + "";
-                grid[r+1][c] = "x";
+                grid[r+1][c] = tileNumber + "";
+                grid[r][c] = "x";
+                emptySpacesLeft++;
             }
 
         }
@@ -147,11 +155,12 @@ function combineTilesLeft(){
     {
         for(var c=0; c < grid[r].length-1; c++)
         {
-            if(c !== grid[r].length-1 && grid[r][c] !== "x" && grid[r][c] == grid[r][c-1])
+            if(c !== grid[r].length-1 && grid[r][c] !== "x" && grid[r][c] == grid[r][c+1])
             {
-                var tileNumber = parseInt(grid[r][c]) + parseInt(grid[r][c-1]);
-                grid[r][c-1] = tileNumber + "";
-                grid[r][c] = "x";
+                var tileNumber = parseInt(grid[r][c]) + parseInt(grid[r][c+1]);
+                grid[r][c] = tileNumber + "";
+                grid[r][c+1] = "x";
+                emptySpacesLeft++;
             }
 
         }
@@ -165,11 +174,12 @@ function combineTilesRight(){
     {
         for(var c=grid[r].length-1; c >= 0; c--)
         {
-            if(c !== 0 && grid[r][c] !== "x" && grid[r][c] == grid[r][c+1])
+            if(c !== 0 && grid[r][c] !== "x" && grid[r][c] == grid[r][c-1])
             {
-                var tileNumber = parseInt(grid[r][c]) + parseInt(grid[r][c+1]);
-                grid[r][c+1] = tileNumber + "";
-                grid[r][c] = "x";
+                var tileNumber = parseInt(grid[r][c]) + parseInt(grid[r][c-1]);
+                grid[r][c] = tileNumber + "";
+                grid[r][c-1] = "x";
+                emptySpacesLeft++;
             }
 
         }
